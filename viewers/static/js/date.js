9fe908f92cf25912d92f03ba3fdd87c0cdb44baf
Ramshid ali <aliramshid@gmail.com>
2020-08-24 19:05:06 +0530

    new static

--- viewers/static/js/date.js
+++
@@ -1,8 +0,0 @@
-var d = new Date();
-        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-        document.getElementById("month").innerHTML = months[d.getMonth()];
-        document.getElementById("date").innerHTML = d.getDate();
-        document.getElementById("year").innerHTML = d.getFullYear();
-
-
-va
